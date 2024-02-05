d7a228c741aa8f464fa1d350ab5a8cb374ff0301
hyungeerhee <hyungeerhee@gmail.com>
2024-02-05 17:52:46 +0900

    no message

--- src/main/java/com/hyungeerhee/spring/ex/SpringExampleApplication.java
+++ src/main/java/com/hyungeerhee/spring/ex/SpringExampleApplication.java
@@ -1,12 +1,12 @@
 package com.hyungeerhee.spring.ex;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
 
 @SpringBootApplication
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+
 public class SpringExampleApplication {
 
 	public static void main(String[] args) {
